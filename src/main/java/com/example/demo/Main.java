2d6514a7fd28c80b767504d1720cf63f067a7c69
kwongwangjae <kgj9906@naver.com>
2024-02-27 02:09:38 +0900

    feat: middle test 2

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,9 +1,35 @@
 package com.example.demo;
 
-public class Main {
-	public static void main(String[] args) {
-		Circle circle = new Circle(5);
-		System.out.println("원의 면적: " + circle.getArea());
+import java.util.*;
+class Main {
+	public Boolean solution(int n){
+		if (n < 2) return false;
 
+		int line = (int) Math.sqrt(n) + 1;
+		for(int i = 2; i < line; i++){
+			if(n % i == 0)return false;
+		}
+		return true;
+	}
+
+
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+
+		System.out.print(T.solution(n));
 	}
 }
+
+
+// package com.example.demo;
+//
+// public class Main {
+// 	public static void main(String[] args) {
+// 		Circle circle = new Circle(5);
+// 		System.out.println("원의 면적: " + circle.getArea());
+//
+// 	}
+// }
+
