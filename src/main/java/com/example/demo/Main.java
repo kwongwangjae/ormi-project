2ac0504086a883d59073ca7fc05281cef2b3d031
kwongwangjae <kgj9906@naver.com>
2024-02-27 02:38:07 +0900

    feat: middle test 4

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,28 +1,76 @@
 package com.example.demo;
 
-import java.text.DecimalFormat;
 import java.util.*;
 class Main {
-	public String solution(int kr) {
-		double usa = 1332.00;
-		double jp = 884.37;
+	public void solution(String len) {
+		String[] words = len.split(" ");
 
-		double usd = kr / usa;
-		double jpy = kr / jp;
+		String longestWord = longWord(words);
+		System.out.println("문자열에서 가장 긴 단어: " + longestWord);
 
-		DecimalFormat decimalFormat = new DecimalFormat("#,###.##");
+		Map<String, Integer> map = new HashMap<>();
+		for(String w : words){
+			map.put(w, map.getOrDefault(w, 0) + 1);
+		}
 
-			return "달러($) = " + decimalFormat.format(usd) + " / JPY = " + decimalFormat.format(jpy);
+		List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(map.entrySet());
+		sortedList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+		System.out.println("문자열에서 가장 많이 나온 단어 Top 3:");
+		int count = 0;
+		for (Map.Entry<String, Integer> entry : sortedList) {
+			if (count >= 3) break;
+			System.out.println(entry.getKey() + ": " + entry.getValue() + "회");
+			count++;
+		}
+	}
+
+
+	private String longWord(String[] words) {
+		String max = "";
+		for (String word : words) {
+			if (word.length() > max.length()) {
+				max = word;
+			}
+		}
+		return max;
 	}
 
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		int kr = kb.nextInt();
+		String len = kb.nextLine();
 
-		System.out.print(T.solution(kr));
+		T.solution(len);
 	}
 }
+
+
+// package com.example.demo;
+//
+// import java.text.DecimalFormat;
+// import java.util.*;
+// class Main {
+// 	public String solution(int kr) {
+// 		double usa = 1332.00;
+// 		double jp = 884.37;
+//
+// 		double usd = kr / usa;
+// 		double jpy = kr / jp;
+//
+// 		DecimalFormat decimalFormat = new DecimalFormat("#,###.##");
+//
+// 			return "달러($) = " + decimalFormat.format(usd) + " / JPY = " + decimalFormat.format(jpy);
+// 	}
+//
+// 	public static void main(String[] args) {
+// 		Main T = new Main();
+// 		Scanner kb = new Scanner(System.in);
+// 		int kr = kb.nextInt();
+//
+// 		System.out.print(T.solution(kr));
+// 	}
+// }
 
 
 // package com.example.demo;
